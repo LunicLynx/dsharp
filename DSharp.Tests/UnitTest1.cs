70ff31fd249153652f3fe1b3b78e308fb6b92942
Florian Dohrendorf <fdohrendorf@outlook.com>
2018-03-29 18:46:46 +0200

    Resolve Tests

--- DSharp.Tests/UnitTest1.cs
+++ DSharp.Tests/UnitTest1.cs
@@ -1,6 +1,7 @@
 using System;
 using System.ComponentModel;
 using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
 using Xunit;
 
 namespace DSharp.Tests
@@ -140,6 +141,99 @@ namespace DSharp.Tests
 
             // Resolve B from inside D finds A.B before C.B
         }
+
+        [Fact]
+        public void ResolveInLocalAssemblyFirst()
+        {
+            /*
+             * assembly A { class B { } }
+             * assembly C { ref A; class B { } }
+             * resolve B from C finds C.B
+             */
+        }
+
+        [Fact]
+        public void AmbiguityTwoReferencesContainingSame()
+        {
+            /*
+             * assembly A { class B { } }
+             * assembly C { class B { } }
+             * assembly D { ref A; ref C; }
+             * resolve B from D ambiguity A.B and C.B
+             */
+        }
+
+        [Fact]
+        public void AmbiguityTwoNamespacesContainingSame()
+        {
+            /*
+             * namespace A { class B { } }
+             * namespace C { class B { } }
+             * namespace D { using A; using C; }
+             * resolve B from D ambiguity A.B and C.B
+             */
+        }
+
+        [Fact]
+        public void ResolveInParent()
+        {
+            /*
+             * namespace A.B { class C { } }
+             * namespace A.B.D { class E : C { } }
+             */
+        }
+
+        [Fact]
+        public void ResolveInParentOfAnotherAssembly()
+        {
+            /*
+             * assembly A { class B { } }
+             * assembly C { ref A; namespace D { class E : B { } } }
+             * resolve B from D, C finds A.B, A
+             */
+        }
+
+        [Fact]
+        public void ResolveInParentOfAnotherAssemblyWithNamespace()
+        {
+            /*
+             * assembly A { namespace B { class C { } } }
+             * assembly D { ref A; namespace B.E { class F : C { } } }
+             * resolve C from B.E, D finds B.C, A
+             */
+
+            // 1. exists B.E.[C], D
+            // 2. exists B.[C], D
+            // 3. exists [C], D
+            // 4. exists only one B.E.[C] in any reference
+            // 5. exists only one B.[C] in any reference -> found
+
+            // 1. exists in current scope
+            // 2. 
+            // 2. exists in parent scope
+            // 3. exists 
+        }
+
+        [Fact]
+        public void ResolveWithAliasUsingBeforeUsing()
+        {
+            /*
+             * namespace A.B.C { class X { } }
+             * namespace H { class G { public class X { } } }
+             * namespace D.E.F { using G = A.B.C; using H; class Y : G.X { } }
+             * resolve G.X from D.E.F finds A.B.C.X
+             */
+        }
+
+        [Fact]
+        public void AmbiguityAliasUsingCollidingWithClass()
+        {
+            /*
+             * namespace A { class X { } }
+             * namespace B { using C = A; class C { public class X { } } class Y : C.X { } }
+             * resolve C.X from B is ambiguise between
+             */
+        }
     }
 
     public class Scope
@@ -147,12 +241,12 @@ namespace DSharp.Tests
 
         public Scope()
         {
-            
+
         }
 
         public Scope(Scope parent)
         {
-            
+
         }
 
         public Scope Add(string name)
